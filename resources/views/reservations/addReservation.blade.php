7635ee8def9e6e27f0023a52ca9362964c6705fa
Maxime Dendauw <maxime@responsestudios.com>
2016-06-18 20:11:59 +0200

    changed name

--- resources/views/reservations/addReservation.blade.php
+++ resources/views/reservations/addReservation.blade.php
@@ -8,19 +8,19 @@
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <div class="form-group">
                   <label for="voornaam">Voornaam</label>
-                  <input type="text" class="form-control" id="voornaam" placeholder="Voornaam">
+                  <input type="text" class="form-control" name="voornaam" placeholder="Voornaam">
                 </div>
                 <div class="form-group">
                   <label for="naam">Naam</label>
-                  <input type="text" class="form-control" id="naam" placeholder="Naam">
+                  <input type="text" class="form-control" name="naam" placeholder="Naam">
                 </div>
                 <div class="form-group">
                   <label for="aantal">Aantal personen</label>
-                  <input type="text" class="form-control" id="aantal" placeholder="Aantal personen">
+                  <input type="text" class="form-control" name="aantal" placeholder="Aantal personen">
                 </div>                     
                 <div class="form-group">
                   <label for="email">Email</label>
-                  <input type="email" class="form-control" id="email" placeholder="Email">
+                  <input type="email" class="form-control" name="email" placeholder="Email">
                 </div>
                 <button type="submit" class="btn btn-default">Versturen</button>
             </form>
