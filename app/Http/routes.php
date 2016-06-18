2e1e14d6ff4a33ec3b3f8e14ac7f9667711e873a
Maxime Dendauw <maxime@responsestudios.com>
2016-06-18 19:56:27 +0200

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,6 +21,6 @@ Route::get('/home', 'HomeController@index');
 
 Route::get('/reservations', 'ReservationsController@index');
 Route::get('/addreservation', 'ReservationsController@add');
-Route::post('/reservations/newreservation', 'ReservationsController@store');
+Route::post('/reservations/{reservation}', 'ReservationsController@store');
 
 /*Route::get('/shops/{shop}', 'ShopController@show');*/
