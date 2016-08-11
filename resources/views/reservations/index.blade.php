16fffdd06b3876b9bb893631220105e7f3846e48
Maxime Dendauw <maxime@responsestudios.com>
2016-08-11 13:42:59 +0200

    new commit

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -1,42 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>All Reservations</h1>
-        
-    @foreach ($reservations as $reservation)
+    <div class="container">
     
-        <div>
-            
-            {{ $reservation->voornaam }} 
-            </br>
-            {{ $reservation->naam }} 
-            </br>   
-            {{ $reservation->aantal }} 
-            </br>
-            {{ $reservation->tel }} 
-            </br>
-            {{ $reservation->email }} 
-            </br>
-            {{ $reservation->opmerkingen }} 
-            </br>
-            {{ $reservation->bevestigd }} 
-            </br>
-            {{ $reservation->reservatiedatum }} 
-            </br>
-            {{ $reservation->reservatietijd }} 
-            </br>
-            <form method="POST" action="/reservations/delete">
-                <input type="hidden" name="_method" value="delete" />
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="hidden" name="id" value="{{ $reservation->id }}">
-                <button type="submit" class="btn btn-default">Verwijderen</button>
-            </form>
-            
-            
-            <hr>
+        <div class="row">   
+            <div class="col-md-12">
+                <h1>All Reservations</h1>
+            </div>
+        </div>
+        
+        @foreach ($reservations as $reservation)
+    
+        <div class="row">
+            <div class="col-md-12">
+        
+                {{ $reservation->voornaam }} 
+                </br>
+                {{ $reservation->naam }} 
+                </br>   
+                {{ $reservation->aantal }} 
+                </br>
+                {{ $reservation->tel }} 
+                </br>
+                {{ $reservation->email }} 
+                </br>
+                {{ $reservation->opmerkingen }} 
+                </br>
+                {{ $reservation->bevestigd }} 
+                </br>
+                {{ $reservation->reservatiedatum }} 
+                </br>
+                {{ $reservation->reservatietijd }} 
+                </br>
+                <form method="POST" action="/reservations/delete">
+                    <input type="hidden" name="_method" value="delete" />
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input type="hidden" name="id" value="{{ $reservation->id }}">
+                    <button type="submit" class="btn btn-default">Verwijderen</button>
+                </form>
+        
+            </div>
             
         </div>        
     
-    @endforeach    
-        
+        @endforeach    
+     
+    </div>    
 @stop
