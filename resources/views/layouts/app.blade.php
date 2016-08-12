99852d5e854894fb9b495326a7a127846fede761
Maxime Dendauw <maxime@responsestudios.com>
2016-08-12 09:40:29 +0200

    new commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,15 +55,15 @@
                               <li><a href="/message/create">Nieuwe boodschap</a></li>
                             </ul>
                     </li>
-                    <li><a href="{{ url('/picture') }}">Afbeeldingen</a></li>
-                    <li><a href="{{ url('/home') }}">Kaart</a></li>
                     <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Uren</a><span class="caret"></span></a>
                             <ul class="dropdown-menu">
                               <li><a href="/hour">Alle openingsuren</a></li>
                               <li><a href="/hour/create">Nieuw openingsuur</a></li>
                             </ul>
-                    </li>    
+                    </li>
+                    <li><a href="{{ url('/picture') }}">Afbeeldingen</a></li>
+                    <li><a href="{{ url('/home') }}">Kaart</a></li>    
                         
                 </ul>
 
