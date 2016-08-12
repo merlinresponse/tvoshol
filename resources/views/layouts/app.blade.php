70949ebbcc2accf2646bb7772a392bfc652b5c62
Maxime Dendauw <maxime@responsestudios.com>
2016-08-12 09:21:15 +0200

    new commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,8 +89,9 @@
     
                 @if (count($errors) > 0)
                     <div class="alert alert-danger">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button
                         <ul>
-                            @foreach ($errors as $error)
+                            @foreach ($errors->all() as $error)
                                 <li>{{ $error }}</li>
                             @endforeach
                         </ul>
