7f5bc12400c088fe9952808843bfb40c3da7ec66
Maxime Dendauw <maxime@responsestudios.com>
2016-08-14 11:02:29 +0200

    new commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
+
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
@@ -27,8 +29,20 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    
+    
+    
+    
     public function index()
     {
-        return view('welcome');
+        
+        $message = Message::orderBy('created_at', 'desc')->take(1)->get();
+        $message_titel = $message->titelNL;
+        $message_tekst = $message->tekstFR;
+        
+    
+        
+        
+        return view('welcome', ['message_titel' => $message_titel, 'message_tekst' => $message_tekst]);
     }
 }
