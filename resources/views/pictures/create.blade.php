619c69389ba7db59d599d72f5b8e3415b142deee
Maxime Dendauw <maxime@responsestudios.com>
2017-02-03 15:19:54 +0100

    new commit

--- resources/views/pictures/create.blade.php
+++ resources/views/pictures/create.blade.php
@@ -25,7 +25,7 @@
                            <label for="avatar">Beeld</label>
                            <input type="file"
                                   class="form-control slim"
-                                  data-ratio="16:9"
+                                  data-ratio="900,400"
                                   id="avatar">
                        </div>
 
