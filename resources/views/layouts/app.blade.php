c2afff08e192712b73d7abb15fc7b64891525f9f
Maxime Dendauw <maxime@responsestudios.com>
2017-02-09 15:48:36 +0100

    new commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -150,12 +150,12 @@
 
 
 
-
+<!--
     <script type="text/javascript" src="/bower_components/jquery/dist/jquery.min.js"></script>
     <script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>
 
     <script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-
+-->
     <script src="/slim/slim.kickstart.min.js"></script>
 
 
