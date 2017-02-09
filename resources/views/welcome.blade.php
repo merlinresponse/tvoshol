dea85e4416e2dcf6119b17802a49b5b3494244e1
Maxime Dendauw <maxime@responsestudios.com>
2017-02-09 20:11:21 +0100

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -265,7 +265,7 @@
               <div class="block">
                 <div class="container text-center app-translate-15" data-transition="entrance">
                   <div class="slider-holder center-block">
-                    <img class="img-responsive" src="{{ asset("img/uploads/" . $picture->bestand) }}">
+                    <img class="img-responsive slider-image" src="{{ asset("img/uploads/" . $picture->bestand) }}">
                   </div>
                 </div>
               </div>
