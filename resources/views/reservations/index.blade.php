b032730a0643bd7721e47cdb27685ec5f235ba5a
Maxime Dendauw <maxime@responsestudios.com>
2017-02-09 13:43:51 +0100

    new commit

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -50,6 +50,7 @@
 
                 <a class="btn btn-small btn-info" href="{{ URL::to('reservation/' . $reservation->id . '/edit') }}">Reservatie wijzigen</a>
                 </br>
+                </br>
                 <form method="POST" action="/reservation/delete">
                     <input type="hidden" name="_method" value="delete" />
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
