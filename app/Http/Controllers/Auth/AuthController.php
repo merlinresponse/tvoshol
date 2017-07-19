fe86e97c6c9d94bb617bf6654659920a79691832
Maxime Dendauw <maxime@responsestudios.com>
2017-07-19 10:21:23 +0200

    new commit

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -63,10 +63,10 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+      /*  return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-        ]);
+        ]); */
     }
 }
